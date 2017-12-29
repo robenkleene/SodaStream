f936adaf704ba1a3c82e1f8c57ff5f5d811fb330
Roben Kleene <robenk@gmail.com>
2017-12-29 10:26:34 -0500

    Add TaskHelper to interface

--- SodaStream/SodaStream.h
+++ SodaStream/SodaStream.h
@@ -17,4 +17,5 @@ FOUNDATION_EXPORT const unsigned char SodaStreamVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SodaStream/PublicHeader.h>
 
 #import "SDATaskRunner.h"
+#import "SDATaskHelper.h"
 #import "NSTask+Termination.h"

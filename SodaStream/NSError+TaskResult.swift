934f25ac148cfb3eb039785ae40030442734d316
Roben Kleene <contact@robenkleene.com>
2018-06-17 16:06:09 -0400

    Remove comma

--- SodaStream/NSError+TaskResult.swift
+++ SodaStream/NSError+TaskResult.swift
@@ -65,7 +65,7 @@ extension NSError {
 
         let userInfo: [String: Any] = [
             NSLocalizedDescriptionKey: description,
-            String(TaskTerminatedUserInfoKey.exitStatus.rawValue): NSNumber(value: Int32(exitCode)),
+            String(TaskTerminatedUserInfoKey.exitStatus.rawValue): NSNumber(value: Int32(exitCode))
         ]
 
         return makeError(userInfo: userInfo, code: TaskTerminatedErrorCode.nonzeroExitStatus.rawValue)

68a69696eb0b671954c44a171154e9d8f40b8001
Roben Kleene <contact@robenkleene.com>
2018-06-17 16:35:26 -0400

    Run autocorrect

--- SodaStream/NSError+SDATaskRunner.swift
+++ SodaStream/NSError+SDATaskRunner.swift
@@ -18,16 +18,16 @@ enum RunCommandPathErrorCode: Int {
 @objc public extension NSError {
     public class func commandPathUnkownError(launchPath: String) -> NSError {
         return makeError(description: "An unkown error occurred running command path: \(launchPath)",
-            code: RunCommandPathErrorCode.unknown.rawValue)
+                         code: RunCommandPathErrorCode.unknown.rawValue)
     }
 
     public class func commandPathUnexecutableError(launchPath: String) -> NSError {
         return makeError(description: "Command path is not executable: \(launchPath)",
-            code: RunCommandPathErrorCode.unexecutable.rawValue)
+                         code: RunCommandPathErrorCode.unexecutable.rawValue)
     }
 
     public class func commandPathExceptionError(launchPath: String) -> NSError {
         return makeError(description: "An exception was thrown running command path: \(launchPath)",
-            code: RunCommandPathErrorCode.exception.rawValue)
+                         code: RunCommandPathErrorCode.exception.rawValue)
     }
 }
